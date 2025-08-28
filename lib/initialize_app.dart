da8e1c9230b8bf79db178c78b8a339bbf33c50b1
Zunair Pervaiz <muhammad.zunair@psca.gop.pk>
2025-08-28 12:29:56 +0500

    Removed unnecessary code

--- lib/initialize_app.dart
+++ lib/initialize_app.dart
@@ -1,19 +1,10 @@
 import 'package:baaba_starter_project/app/app.dart';
-import 'package:baaba_starter_project/app/enums/env.dart';
 import 'package:baaba_starter_project/di/dependency_injection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void initializeApp(Env env) async {
+void initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Future<void> initializeEnvironmentConfig() async {
-  //   final filename = switch (env) {
-  //     Env.dev => Assets.env.aEnvDev,
-  //     Env.prod => Assets.env.aEnvProd,
-  //   };
-  //   await dotenv.load(fileName: filename);
-  // }
 
   await Future.wait([
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]),
